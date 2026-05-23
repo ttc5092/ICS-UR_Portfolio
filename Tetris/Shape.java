d40f90cd4737c274eab22369b223d598750d62bb
Alex M <minediamonds4011@gmail.com>
2026-05-23 15:11:15 -0400

    Create Shape.java

---
+++ Tetris/Shape.java
@@ -0,0 +1,78 @@
+import java.util.Random;
+
+//THis class represents the Tetrominoes and handles movement and rotation
+
+public class Shape extends BasicEnt {
+
+    enum Tetrominoes { NoShape, ZShape, SShape, LineShape, TShape, SquareShape, LShape, MirroredLShape }
+    
+    private Tetrominoes pieceShape;
+    private int[][] coords;
+    
+    public void setX(int index, int x) { coords[index][0] = x; }
+    public void setY(int index, int y) { coords[index][1] = y; }
+    public int x(int index) { return coords[index][0]; }
+    public int y(int index) { return coords[index][1]; }
+
+    public Shape() {
+        coords = new int[4][2];
+        setShape(Tetrominoes.NoShape);
+    }
+
+    public void setShape(Tetrominoes shape) {
+        // Defines coordinates [x, y] for all 7 standard pieces
+        int[][][] coordsTable = new int[][][]{
+                { { 0, 0 },   { 0, 0 },   { 0, 0 },   { 0, 0 } },
+                { { 0, -1 },  { 0, 0 },   { -1, 0 },  { -1, 1 } },
+                { { 0, -1 },  { 0, 0 },   { 1, 0 },   { 1, 1 } },
+                { { 0, -1 },  { 0, 0 },   { 0, 1 },   { 0, 2 } },
+                { { -1, 0 },  { 0, 0 },   { 1, 0 },   { 0, 1 } },
+                { { 0, 0 },   { 1, 0 },   { 0, 1 },   { 1, 1 } },
+                { { -1, -1 }, { 0, -1 },  { 0, 0 },   { 0, 1 } },
+                { { 1, -1 },  { 0, -1 },  { 0, 0 },   { 0, 1 } }
+        };
+
+        for (int i = 0; i < 4; i++) {
+            System.arraycopy(coordsTable[shape.ordinal()][i], 0, coords[i], 0, 2);
+        }
+        pieceShape = shape;
+    }
+
+    public Tetrominoes getShape() { return pieceShape; }
+
+    public void setRandomShape() {
+        Random r = new Random();
+        int x = Math.abs(r.nextInt()) % 7 + 1;
+        setShape(Tetrominoes.values()[x]);
+    }
+
+    public int minY() {
+        int m = coords[0][1];
+        for (int i = 0; i < 4; i++) {
+            m = Math.min(m, coords[i][1]);
+        }
+        return m;
+    }
+
+    public Shape rotateLeft() {
+        if (pieceShape == Tetrominoes.SquareShape) return this;
+        Shape result = new Shape();
+        result.pieceShape = pieceShape;
+        for (int i = 0; i < 4; ++i) {
+            result.setX(i, y(i));
+            result.setY(i, -x(i));
+        }
+        return result;
+    }
+
+    public Shape rotateRight() {
+        if (pieceShape == Tetrominoes.SquareShape) return this;
+        Shape result = new Shape();
+        result.pieceShape = pieceShape;
+        for (int i = 0; i < 4; ++i) {
+            result.setX(i, -y(i));
+            result.setY(i, x(i));
+        }
+        return result;
+    }
+}
